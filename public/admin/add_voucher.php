db0fde5f5b55fe19c2bd01a7fcf577a4f96ea08a
syamxm <ahmadsyamim200@gmail.com>
2026-07-14 02:01:35 +0800

    Clamp voucher discount value to 0-100

--- public/admin/add_voucher.php
+++ public/admin/add_voucher.php
@@ -18,7 +18,7 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   csrf_verify();
   $code = trim($_POST['code']);
-  $discount = floatval($_POST['discount_value']);
+  $discount = max(0, min(100, floatval($_POST['discount_value'])));
   $valid_from = $_POST['valid_from'];
   $valid_until = $_POST['valid_until'];
   $status = $_POST['status'];
